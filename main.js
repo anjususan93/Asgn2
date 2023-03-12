24510d9eef99c731959716e2d9fb39c19e02fd2f
anjususan93 <127041551+anjususan93@users.noreply.github.com>
2023-03-12 16:31:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,21 +7,21 @@ var y = 0;
 var speed = 5;
 
 document.addEventListener('keydown', function(event) {
-if (event.keyCode === 37) { // left arrow key
-x -= speed;
-} else if (event.keyCode === 38) { // up arrow key
-y -= speed;
-} else if (event.keyCode === 39) { // right arrow key
-x += speed;
-} else if (event.keyCode === 40) { // down arrow key
-y += speed;
-}
+  if (event.keyCode === 37) { // left arrow key
+    x -= speed;
+  } else if (event.keyCode === 38) { // up arrow key
+    y -= speed;
+  } else if (event.keyCode === 39) { // right arrow key
+    x += speed;
+  } else if (event.keyCode === 40) { // down arrow key
+    y += speed;
+  }
 
-document.getElementById('pacman').style.left = x + 'px';
-document.getElementById('pacman').style.top = y + 'px';
-
-// Having music//
-var audio = document.getElementByld("audio");
-audio.play();
-audio.loop = true;
+  document.getElementById('pacman').style.left = x + 'px';
+  document.getElementById('pacman').style.top = y + 'px';
+  
+  // Having music//
+  var audio = document.getElementById("audio");
+  audio.play();
+  audio.loop = true;
 });
